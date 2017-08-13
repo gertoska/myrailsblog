060980cf20c21bfaf8374fe8d1a64bbdf825b2a2
gertoska <tosk1@protonmail.com>
2017-08-13 15:50:29 +0200

    create categories files automatically using scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811111021) do
+ActiveRecord::Schema.define(version: 20170813124406) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
