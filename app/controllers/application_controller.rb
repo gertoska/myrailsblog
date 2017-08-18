2996810b7a248b69f28e79880c3307a2c8fb4e9a
gertoska <tosk1@protonmail.com>
2017-08-18 10:48:53 +0200

    search post by title or body

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :all_categories
   helper_method :all_posts
+  before_filter :site_search
 
   def all_categories
     @categories = Category.all
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def all_posts
     @posts = Post.all
+  end
+
+  def site_search
+    @search = Post.search(params[:search])
+    @search_posts = @search.result
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if request.query_parameters[:q].present?
+      search = request.query_parameters[:q][:title_or_body_contains]
+      @posts = Post.where('title LIKE :search OR body LIKE :search', search: "%#{search}%");
+    else
+      @posts = Post.all
+    end
   end
 
   def show
