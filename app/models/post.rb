5876ab9855c10022ec83d9ea6cf03a0e0ad7d405
gertoska <tosk1@protonmail.com>
2017-08-19 19:28:55 +0200

    add comment system

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :admin_user
+  has_many :comments, :dependent => :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818075326) do
+ActiveRecord::Schema.define(version: 20170819170017) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -53,13 +53,24 @@ ActiveRecord::Schema.define(version: 20170818075326) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "category_id"
-    t.integer  "author_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "admin_user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
